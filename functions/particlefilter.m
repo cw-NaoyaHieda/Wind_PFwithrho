ac990878448f389dd298a4f82e0bf226885434a6
naoya12185 <hero12185@gmail.com>
2018-10-05 12:34:18 +0900

    goog filter

--- functions/particlefilter.m
+++ functions/particlefilter.m
@@ -38,12 +38,12 @@ function [pfOut1, pfOut2, wt, pfOut1_mean, pfOut2_mean] = particlefilter(par, y,
     %時点itでのParticle発生
     pfOut1(it,:) = phi1 * pfOut1(it - 1, :) + randn(1, nParticle)*sqrt(1-phi1^2);
     rho1(it-1,:) = 0.95 * ( tanh( sig_rho * pfOut1(it-1,:) + mu_rho)+1) / 2;
-    pfOut2(it,:) = arrayfun(@(x,y) r_conditional_WJ(1, x, mu_g, y, mu_f, rho_f, 1), pfOut2(it-1,:), rho1(it-1,:));
+    pfOut2(it,:) = gpuArray(arrayfun(@(x,y) r_conditional_WJ(1, x, mu_g, y, mu_f, rho_f, 1), gather(pfOut2(it-1,:)), gather(rho1(it-1,:))));
     %pfOut2(it,:) = arrayfun(@(x) pi_shori(x), pfOut2(it,:));
     %発生したParticleのweight
-    tmp1 = arrayfun(@(theta, rho_g) d_conditional_WJ(y(it-1), theta, mu_g, rho_g, mu_f, rho_f, 1), pfOut2(it-1,:), rho1(it-1,:));
+    tmp1 = gpuArray(arrayfun(@(theta, rho_g) d_conditional_WJ(y(it-1), theta, mu_g, rho_g, mu_f, rho_f, 1),gather(pfOut2(it-1,:)), gather(rho1(it-1,:))));
 
-    tmp2 = arrayfun(@(x) gampdf(v(it-1)/(gam*exp(x/2)) , V, 1/V)/(gam*exp(x/2)), pfOut1(it,:));
+    tmp2 = gpuArray(arrayfun(@(x) gampdf(v(it-1)/(gam*exp(x/2)) , V, 1/V)/(gam*exp(x/2)), gather(pfOut1(it,:))));
 
     wt(it,:) = (tmp1/sum(tmp1)) .* (tmp2/sum(tmp2)) .* wt(it-1,:);
 
@@ -52,16 +52,15 @@ function [pfOut1, pfOut2, wt, pfOut1_mean, pfOut2_mean] = particlefilter(par, y,
     %[pfOut1(it,:), pfOut2(it,:)] = Resample2(pfOut1(it,:), pfOut2(it,:), wt(it,:), nParticle);
     [pfOut1(it,:), pfOut2(it,:),wt(it,:)] = Resample2_wt(pfOut1(it,:), pfOut2(it,:), wt(it,:), nParticle);
     wt(it,:) = wt(it,:) / sum(wt(it,:));
-    N_eff(it) = 1 / (wt(it,:) * wt(it,:)');
+    N_eff(it) = gather(1 / (wt(it,:) * wt(it,:)'));
     if  N_eff(it) < nEff
-      it
       [pfOut1(it,:), pfOut2(it,:)] = Resample2(pfOut1(it,:), pfOut2(it,:), wt(it,:), nParticle);
       wt(it,:) = 1 / nParticle;
     end
 
     pfOut1_mean(it) = gather(pfOut1(it,:) * wt(it,:)');
     pfOut2_mean(it) = gather(pfOut2(it,:) * wt(it,:)');
-
+    it
   end
 
 
